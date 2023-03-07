2e5fdef62393c1aaecce5de9df82d754ad2d0593
iliya <iliya@DESKTOP-IABB2TF>
2023-03-07 14:11:14 +0300

    Init

---
+++ SecondSemester-First/SecondSemester-First.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char* str = new char[256];
+	char* str2 = new char[256];
+
+	cout << "\nEnter:" << endl;
+
+	cin.ignore();
+
+	cin.getline(str, 256);
+
+	cout << "\nEnter 2:" << endl;
+
+	cin.getline(str2, 256);
+
+	cout << str << " " << str2 << endl;
+}
